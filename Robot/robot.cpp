f092387936b1f35641c025401eea70698eefd156
Abdoul Hanane <gbadhanane@gmail.com>
2020-09-30 14:49:17 +0000

    simplified code for reading

--- Robot/robot.cpp
+++ Robot/robot.cpp
@@ -10,14 +10,10 @@ void Robot::print() const{
   cout<<"I am at  ";
   rPos.print();
   cout<<" and I am facing ";
-      if (orientation==east)
-      cout<<"EAST\n";
-    else if(orientation==south)
-      cout<<"Sout\n";
-    else if(orientation==west)
-    cout<<"WEST\n";
-    else if(orientation==north)
-    cout<<"NORTH\n";
+      if (orientation==east)cout<<"EAST\n";
+      else if(orientation==south)cout<<"SOUTH\n";
+      else if(orientation==west)cout<<"WEST\n";
+      else if(orientation==north)cout<<"NORTH\n";
 }
 //function to set the new orientation
 void Robot::setOrientation(orientation_type orientation){
@@ -26,35 +22,26 @@ void Robot::setOrientation(orientation_type orientation){
 //function to return true if the robot can move forward
 bool Robot::forward(){
   bool move =  false;
-  if(orientation==east){
-      if(rPos.getX()<9){
-        rPos.set(rPos.getX()+1,rPos.getY());
-        move = true;
-      }
+  if(orientation==east && rPos.getX()<9){
+      rPos.set(rPos.getX()+1,rPos.getY());
+      move = true;
   }
-  else if(orientation==west){
-      if(rPos.getX()>0){
-        rPos.set(rPos.getX()-1, rPos.getY());
-        move = true;
-      }
+  else if(orientation==west&&rPos.getX()>0){
+      rPos.set(rPos.getX()-1, rPos.getY());
+      move = true;  
   }
-  else if(orientation==north){
-    if (rPos.getY()<9){
+  else if(orientation==north && rPos.getY()<9 ){
        rPos.set(rPos.getX(),rPos.getY()+1);
        move = true;
-  } 
-}
-else if(orientation==south){
-  if(rPos.getY()>0){
-    rPos.set(rPos.getX(),rPos.getY()-1);
-    move = true;
   }
-}
+  else if(orientation==south && rPos.getY()>0){
+      rPos.set(rPos.getX(),rPos.getY()-1);
+      move = true;
+  }
 return move;
 }
 // turn clocewise if the robot reaches the wall
-void Robot::turnCW()
-{
+void Robot::turnCW(){
     if(orientation==east)
     orientation=south;
     else if(orientation==south)
@@ -63,7 +50,6 @@ void Robot::turnCW()
     orientation=north;
     else if(orientation==north)
     orientation=east;
-
 }
 //turn anticlockwise if the robot reach the wall
 void Robot::turnAntiCW()
@@ -98,27 +84,25 @@ bool Robot::westEnd()
   return (rPos.getX()==0);
 }
 //return true if 
-bool Robot::zag()
-{
-  if(eastEnd()){
-    turnAntiCW();
-    forward();
-    turnAntiCW();
-    return true;
-  }
-  else
-   return false;
+bool Robot::zag(){
+  bool ok = false;
+    if(eastEnd()){
+      turnAntiCW();
+      forward();
+      turnAntiCW();
+      ok= true;
+    }
+  return ok;
 }
-bool Robot::zig()
-{
-  if(westEnd()){
-    turnCW();
-    forward();
-    turnCW();
-    return true;
-  }
-  else 
-    return false;
+bool Robot::zig(){
+  bool ok= false;
+    if(westEnd()){
+      turnCW();
+      forward();
+      turnCW();
+      ok = true;
+    }
+  return ok;
 }
 int Robot:: getX(){
   return rPos.getX();
